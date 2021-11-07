bcd350c2c7f5e5a03585fc821f129fbc626a5314
Ashish Gupta <ashgupta1489@gmail.com>
2021-11-07 20:39:03 +0530

    Added Bootstrap Gradle Project
    Added Bootstrap gradle project

---
+++ src/main/java/org/bits/oss/OssGroup8Application.java
@@ -0,0 +1,13 @@
+package org.bits.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssGroup8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssGroup8Application.class, args);
+	}
+
+}
